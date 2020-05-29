3318e66fc261122926b16b1720e38d9e59750dc5
Cuong <bdcuong91@gmail.com>
2020-05-30 00:13:43 +0200

    change db script

--- db/db_scripts/Script-7.sql
+++ db/db_scripts/Script-7.sql
@@ -1,13 +1,7 @@
 select * from mychat.users u ;
 delete from mychat.users where user_name="Manuel";
 
+update mychat.users  set forgotten_answer = "CD" where user_name = "Cuong";
+
 select * from mychat.users_chats;
 delete from mychat.users_chats;
-
-INSERT INTO mychat.users_chats
-(sender_username, receiver_username, msg_content, msg_status, msg_date)
-VALUES('Cuong', 'Manuel', 'Hallo', 'read', CURRENT_TIMESTAMP);
-
-INSERT INTO mychat.users_chats
-(sender_username, receiver_username, msg_content, msg_status, msg_date)
-VALUES('Manuel', 'Cuong', 'Wie gehts?', 'unread', CURRENT_TIMESTAMP);
